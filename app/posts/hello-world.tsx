c1d15b1175e27280422338265c0fa478bb2e0cd6
btja <brian.w.tjahjadi@gmail.com>
2025-12-28 23:34:17 +1100

    setup

--- app/posts/hello-world.tsx
+++ app/posts/hello-world.tsx
@@ -1,4 +1,6 @@
 import type { PostMeta } from './posts';
+import { useShikiHighlighter } from 'react-shiki';
+import { underlineAnnotatedLines } from './shikiTransformers';
 
 export const meta: PostMeta = {
   title: 'Hello World',
@@ -7,9 +9,20 @@ export const meta: PostMeta = {
 };
 
 export default function Post() {
+  const code = `fn test() {
+  no; // [!test]
+}`;
+
+  const highlightedCode = useShikiHighlighter(code, 'rust', 'vitesse-dark', {
+    transformers: [underlineAnnotatedLines()],
+  });
+
   return (
     <>
       <p>Hey, you're not supposed to see this!</p>
+      <code className="p-5 bg-[#121212] rounded mb-5 mt-5">
+        {highlightedCode}
+      </code>
     </>
   );
 }
